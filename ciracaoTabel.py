ffc24c1d935d0e007b0575a48e9161c95cfd6ec1
Thiago0029 <ti.thiagoporto@gmail.com>
2023-09-29 05:03:39 -0300

    adicioando parametros da tavela

---
+++ ciracaoTabel.py
@@ -0,0 +1,10 @@
+'''
+    Nome: criacaoTabela
+    Autor: Thiago
+    Função: criação da tabela
+'''
+
+from conexao import *
+import psycopg2
+
+cur.execute ("CREATE TABLE alimentos(PRIMARY KEY id INT(3), nome VARCHAR(100), valor FLOAT(2.2))")
