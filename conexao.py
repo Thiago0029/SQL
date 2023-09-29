0ff2534fc2a66447a532244b98b90b72a22fd2bd
Thiago0029 <ti.thiagoporto@gmail.com>
2023-09-29 05:15:52 -0300

    tratamento de erro de conexao com try except

--- conexao.py
+++ conexao.py
@@ -4,12 +4,20 @@
     Função: Realizar a conexão com o banco de dados PostgreSQL
 '''
 import psycopg2
+from psycopg2 import OperationalError
+try:
+    conexao = psycopg2.connect(
+        dbname = "estudos",
+        user =  "postgresql",
+        passwords = "123",
+        host = "192.168.1.13"
+    )
 
-conexao = psycopg2.connect(
-    dbname = "estudos",
-    user =  "postgresql",
-    passwords = "123",
-    host = "192.168.1.13"
-)
+    cur = conexao.cursor()
 
-cur = conexao.cursor()
+except OperationalError as e:
+    print(f"Erro ao conectar ao PostgreSQL: {e}")
+
+finally:
+    if conexao:
+        conexao.close()
