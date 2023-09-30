55923350159760b235da2387dc4473ea5a2d0ea4
Thiago0029 <ti.thiagoporto@gmail.com>
2023-09-30 02:13:04 -0300

    cometarios adiconados

--- ciracaoTabela.py
+++ ciracaoTabela.py
@@ -4,8 +4,8 @@
     Função: criação da tabela
 '''
 
-from conexao import cur
-import psycopg2
+from conexao import cur # iomporta a função cur do arquivo conexao
+import psycopg2 # drive
 
-cur.execute("CREATE TABLE alimentos(id INT PRIMARY KEY, nome VARCHAR, valor FLOAT)")
-print("Tabela criada")
+cur.execute("CREATE TABLE alimentos(id INT PRIMARY KEY, nome VARCHAR, valor FLOAT)") # executa uma CRUD
+print("Tabela criada") #exibe mensgem caso tabela seja craida
--- conexao.py
+++ conexao.py
@@ -3,20 +3,20 @@
     Autor: Thiago
     Função: Realizar a conexão com o banco de dados PostgreSQL
 '''
-import psycopg2
-from psycopg2 import OperationalError
+import psycopg2 # importa o drive psycopg2 para conexão com o POStgreSQL
+from psycopg2 import OperationalError # importa função para tratamento de erro 
 
-try:
-    conexao = psycopg2.connect(
-        dbname = "estudos",
-        user =  "postgres",
-        password = '123',
-        host = '192.168.1.7',
-        port = '5432'
+try: # funcionamento normal do códio
+    conexao = psycopg2.connect( # salva os dados para conexão com Postgresql
+        dbname = "estudos", #
+        user =  "postgres", #
+        password = '123', # dados da conexão
+        host = '192.168.1.7', #
+        port = '5432' #
     )
 
-    cur = conexao.cursor()
-    print ("Conexão ok")
+    cur = conexao.cursor() # inicia a conexão
+    print ("Conexão ok") # exibi confirmação da conexão
 
-except OperationalError as e:
-    print(f"Erro ao conectar ao PostgreSQL: {e}")
+except OperationalError as e: # chamda caso ocorra erro para efetuar conexão
+    print(f"Erro ao conectar ao PostgreSQL: {e}") # exibi mensagem contendo a mensagem de erro
