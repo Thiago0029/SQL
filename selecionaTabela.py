ad462509f45abdb6601df3fd813cb8af440bae19
Thiago0029 <ti.thiagoporto@gmail.com>
2023-09-30 03:40:13 -0300

    criação do arquivo selecionaTabela

---
+++ selecionaTabela.py
@@ -0,0 +1,14 @@
+'''
+    Nome: craicaoTabela
+    Author: Thiago
+    Função: Seleciona tabela 
+'''
+
+import psycopg2
+from psycopg2 import OperationalError
+from conexao import cur
+
+try:
+    cur.exeute("SELEC INTO tabela (coluna) values (%s)")
+except OperationalError as e:
+    print(f"Erro: {e}")
